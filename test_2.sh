4d2d08d579028d40845194ad78a012d71e33704a
RubyK777 <r7kong@uwaterloo.ca>
2021-03-01 12:44:55 -0500

    Use previous code with a for loop instead of the new one

--- test_2.sh
+++ test_2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --output=positive_DC1_9914_1e-3.out
+#SBATCH --output=negative_DC1_9914_1e-3.out
 #SBATCH --time=06:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=r7kong@uwaterloo.ca
 
 module load octave
 
-octave --silent --no-gui test.m
+octave --silent --no-gui test_2.m
